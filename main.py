38571a9d82ce79c127547100f369e0ffe7266896
Junyu11111 <john2double@gmail.com>
2023-11-21 21:59:00 -0500

    Added best fit.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 from matplotlib import pyplot as plt
+from Analysis_Module import *
 from scipy.optimize import curve_fit
 
 
@@ -30,8 +31,9 @@ def velocities_generator(times: list, posns: list):
 
 def quick_plot(x: list, y: list, title: str):
     plt.figure(title)
-    plt.plot(x, y, marker="o", ls='')
+    plt.plot(x, y, marker="o", ls='', label=title)
     plt.title(title)
+    plt.legend()
 
 
 def get_index_geq(lst, value):
@@ -64,24 +66,42 @@ def get_ball_width(file_path):
     width_dict = {}
     ball_num, m1, m2, m3 = np.loadtxt(file_path, delimiter=',', unpack=True,
                                       skiprows=2)
-    mean_width = np.mean((m1, m2, m3), axis=0)
-    mean_width_err = np.std((m1, m2, m3), axis=0) / np.sqrt(3)
+    mean_width = np.mean((m1, m2, m3), axis=0) / 1000
+    mean_width_err = np.std((m1, m2, m3), axis=0) / 1000 * np.sqrt(3)
     for i in range(len(ball_num)):
-        width_dict["B{}".format(ball_num[i])] = (
+        width_dict["B{}".format(int(ball_num[i]))] = (
             mean_width[i], mean_width_err[i])
     return width_dict
 
 
-def v_correct(v_dict, w_dict, dim):
+def v_correct(v_dict, w_dict, dim, dim_err):
     v_corr_dict = {}
     for ii in v_dict:
-        v_corr_dict[ii] = v_dict[ii] / (
-                    1 - 2.104 * w_dict[ii] / dim + 2.089 * (w_dict / dim) ** 2)
+        lst = []
+        dd = w_dict[ii][0] / dim
+        dd_err = error_prop_multiplication(dd, [w_dict[ii], [dim, dim_err]])
+        divisor = (1 - 2.104 * dd + 2.089 * dd ** 2)
+        divisor_err = error_prop_addition([2.104 * dd_err, error_prop_exponent(dd, dd_err, 2)])
+        v_corr = v_dict[ii][0] / (1 - 2.104 * dd + 2.089 * dd ** 2)
+        lst.append(v_corr)
+        lst.append(
+            error_prop_multiplication(v_corr, [w_dict[ii], [divisor, divisor_err]])
+        )
+        v_corr_dict[ii] = lst
     return v_corr_dict
+
+
+def inverse_function(x, a):
+    return a * x ** 2
+
+
+def inverse_sqrt_function(x, a):
+    return a * np.sqrt(x)
 
 
 if __name__ == "__main__":
     dimension_of_container = 9.5 / 100
+    dimension_of_container_err = 0.1 / 100
 
     water_time_dict = {'B1': (1, 3), 'B2': (1, 2), 'B3': (1, 2),
                        'B4': (0.75, 1.75), 'B5': (1.2, 1.8)}
@@ -89,84 +109,99 @@ if __name__ == "__main__":
                           'B4': (2, 6), 'B5': (1, 4)}
     water_width_dict = get_ball_width(
         "Ball Widths.xlsx - Ball Widths Glycerin.csv")
-    print(velocities_from_data_sets("Water_data", water_time_dict))
     print(water_width_dict)
-    # glycerin ball 1
-    g_b1_times, g_b1_positions = time_positions_extractor(
-        "Glycerin_data/GabrielleJunyuGlycerinNov142023_B1.txt")
-    g_b1_velocities = velocities_generator(g_b1_times, g_b1_positions)
-
-    # print(g_b1_velocities)
-    quick_plot(g_b1_times[20:], g_b1_velocities[20:], "g ball 1")
-
-    # glycerin ball 2
-    g_b2_times, g_b2_positions = time_positions_extractor(
-        "Glycerin_data/GabrielleJunyuGlycerinNov142023_B2.txt")
-    g_b2_velocities = velocities_generator(g_b2_times, g_b2_positions)
-
-    quick_plot(g_b2_times, g_b2_velocities, "g ball 2")
-    # print(g_b2_velocities)
-
-    # glycerin ball 3
-    g_b3_times, g_b3_positions = time_positions_extractor(
-        "Glycerin_data/GabrielleJunyuGlycerinNov142023_B3.txt")
-    g_b3_velocities = velocities_generator(g_b3_times, g_b3_positions)
-
-    quick_plot(g_b3_times, g_b3_velocities, "g ball 3")
-    # print(g_b3_velocities)
-
-    # glycerin ball 4
-    g_b4_times, g_b4_positions = time_positions_extractor(
-        "Glycerin_data/GabrielleJunyuGlycerinNov142023_B4.txt")
-    g_b4_velocities = velocities_generator(g_b4_times, g_b4_positions)
-
-    quick_plot(g_b4_times, g_b4_velocities, "g ball 4")
-    # print(g_b2_velocities)
-
-    # glycerin ball 5
-    g_b5_times, g_b5_positions = time_positions_extractor(
-        "Glycerin_data/GabrielleJunyuGlycerinNov142023_B5.txt")
-    g_b5_velocities = velocities_generator(g_b5_times, g_b5_positions)
-
-    quick_plot(g_b5_times, g_b5_velocities, "g ball 5")
-    # print(g_b5_velocities)
-
-    # water ball 1
-    w_b1_times, w_b1_positions = time_positions_extractor(
-        "Water_data/GabrielleJunyuWaterNov22023_B1.txt")
-    w_b1_velocities = velocities_generator(w_b1_times, w_b1_positions)
-
-    quick_plot(w_b1_times[1:], w_b1_velocities[1:], "w ball 1")
-    # print(w_b1_velocities)
-
-    # water ball 2
-    w_b2_times, w_b2_positions = time_positions_extractor(
-        "Water_data/GabrielleJunyuWaterNov22023_B2.txt")
-
-    w_b2_velocities = velocities_generator(w_b2_times, w_b2_positions)
-
-    quick_plot(w_b2_times[1:], w_b2_velocities[1:], "w ball 2")
-    # print(w_b2_velocities)
-
-    # water ball 3
-    w_b3_times, w_b3_positions = time_positions_extractor(
-        "Water_data/GabrielleJunyuWaterNov22023_B3.txt")
-    w_b3_velocities = velocities_generator(w_b3_times, w_b3_positions)
-
-    quick_plot(w_b3_times[1:], w_b3_velocities[1:], "w ball 3")
-
-    # water ball 4
-    w_b4_times, w_b4_positions = time_positions_extractor(
-        "Water_data/GabrielleJunyuWaterNov22023_B4.txt")
-    w_b4_velocities = velocities_generator(w_b4_times, w_b4_positions)
-
-    quick_plot(w_b4_times[1:], w_b4_velocities[1:], "w ball 4")
-
-    # water ball 5
-    w_b5_times, w_b5_positions = time_positions_extractor(
-        "Water_data/GabrielleJunyuWaterNov22023_B5.txt")
-    w_b5_velocities = velocities_generator(w_b5_times, w_b5_positions)
-
-    quick_plot(w_b5_times[1:], w_b5_velocities[1:], "w ball 5")
-    # print(w_b5_velocities)
+    v_dict = v_correct(velocities_from_data_sets("Water_data", water_time_dict), water_width_dict,
+                       dimension_of_container, dimension_of_container_err)
+    print(v_dict)
+    v_lst = []
+    v_err_lst = []
+    w_lst = []
+    w_err_lst = []
+    for i in water_width_dict:
+        v_lst.append(v_dict[i][0])
+        v_err_lst.append(v_dict[i][1])
+        w_lst.append(water_width_dict[i][0])
+        w_err_lst.append(water_width_dict[i][1])
+    print(v_lst)
+    plot_x_vs_y(w_lst, w_err_lst, v_lst, v_err_lst, "water", inverse_function)
+    plot_x_vs_y(w_lst, w_err_lst, v_lst, v_err_lst, "water", inverse_sqrt_function)
     plt.show()
+    # # glycerin ball 1
+    # g_b1_times, g_b1_positions = time_positions_extractor(
+    #     "Glycerin_data/GabrielleJunyuGlycerinNov142023_B1.txt")
+    # g_b1_velocities = velocities_generator(g_b1_times, g_b1_positions)
+    #
+    # # print(g_b1_velocities)
+    # quick_plot(g_b1_times[20:], g_b1_velocities[20:], "g ball 1")
+    #
+    # # glycerin ball 2
+    # g_b2_times, g_b2_positions = time_positions_extractor(
+    #     "Glycerin_data/GabrielleJunyuGlycerinNov142023_B2.txt")
+    # g_b2_velocities = velocities_generator(g_b2_times, g_b2_positions)
+    #
+    # quick_plot(g_b2_times, g_b2_velocities, "g ball 2")
+    # # print(g_b2_velocities)
+    #
+    # # glycerin ball 3
+    # g_b3_times, g_b3_positions = time_positions_extractor(
+    #     "Glycerin_data/GabrielleJunyuGlycerinNov142023_B3.txt")
+    # g_b3_velocities = velocities_generator(g_b3_times, g_b3_positions)
+    #
+    # quick_plot(g_b3_times, g_b3_velocities, "g ball 3")
+    # # print(g_b3_velocities)
+    #
+    # # glycerin ball 4
+    # g_b4_times, g_b4_positions = time_positions_extractor(
+    #     "Glycerin_data/GabrielleJunyuGlycerinNov142023_B4.txt")
+    # g_b4_velocities = velocities_generator(g_b4_times, g_b4_positions)
+    #
+    # quick_plot(g_b4_times, g_b4_velocities, "g ball 4")
+    # # print(g_b2_velocities)
+    #
+    # # glycerin ball 5
+    # g_b5_times, g_b5_positions = time_positions_extractor(
+    #     "Glycerin_data/GabrielleJunyuGlycerinNov142023_B5.txt")
+    # g_b5_velocities = velocities_generator(g_b5_times, g_b5_positions)
+    #
+    # quick_plot(g_b5_times, g_b5_velocities, "g ball 5")
+    # # print(g_b5_velocities)
+    #
+    # # water ball 1
+    # w_b1_times, w_b1_positions = time_positions_extractor(
+    #     "Water_data/GabrielleJunyuWaterNov22023_B1.txt")
+    # w_b1_velocities = velocities_generator(w_b1_times, w_b1_positions)
+    #
+    # quick_plot(w_b1_times[1:], w_b1_velocities[1:], "w ball 1")
+    # # print(w_b1_velocities)
+    #
+    # # water ball 2
+    # w_b2_times, w_b2_positions = time_positions_extractor(
+    #     "Water_data/GabrielleJunyuWaterNov22023_B2.txt")
+    #
+    # w_b2_velocities = velocities_generator(w_b2_times, w_b2_positions)
+    #
+    # quick_plot(w_b2_times[1:], w_b2_velocities[1:], "w ball 2")
+    # # print(w_b2_velocities)
+    #
+    # # water ball 3
+    # w_b3_times, w_b3_positions = time_positions_extractor(
+    #     "Water_data/GabrielleJunyuWaterNov22023_B3.txt")
+    # w_b3_velocities = velocities_generator(w_b3_times, w_b3_positions)
+    #
+    # quick_plot(w_b3_times[1:], w_b3_velocities[1:], "w ball 3")
+    #
+    # # water ball 4
+    # w_b4_times, w_b4_positions = time_positions_extractor(
+    #     "Water_data/GabrielleJunyuWaterNov22023_B4.txt")
+    # w_b4_velocities = velocities_generator(w_b4_times, w_b4_positions)
+    #
+    # quick_plot(w_b4_times[1:], w_b4_velocities[1:], "w ball 4")
+    #
+    # # water ball 5
+    # w_b5_times, w_b5_positions = time_positions_extractor(
+    #     "Water_data/GabrielleJunyuWaterNov22023_B5.txt")
+    # w_b5_velocities = velocities_generator(w_b5_times, w_b5_positions)
+    #
+    # quick_plot(w_b5_times[1:], w_b5_velocities[1:], "w ball 5")
+    # # print(w_b5_velocities)
+    # plt.show()
